82d562cc6dacd14435a8a7896c2e3f25b1f287b9
Sahil <sahilsinhmar07@gmail.com>
2023-07-14 17:44:54 +0530

    fixes

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,25 +1,22 @@
-import { connect } from "@/dbConfig/dbCongifg";
-import User from "@/models/userModel";
-import { NextRequest,NextResponse } from "next/server";
 import { getDataFromToken } from "@/helpers/getDatafromToken";
 
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbCongifg";
 
 connect();
 
 export async function GET(request:NextRequest){
-  try {
-    const userId=getDataFromToken(request);
 
-    const user =await User.findOne({_id:userId}).
-    select("-passwrod")
-    
-    return NextResponse.json({
-      message:"user found",
-      data:user
-    })
-  } catch (error:any) {
-    NextResponse.json({
-      error:error.message
-    })
-  }
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({_id: userId}).select("-password");
+        return NextResponse.json({
+            mesaaage: "User found",
+            data: user
+        })
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 400});
+    }
+
 }
